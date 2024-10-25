29a40c0dfd4f82a694f2046a2cf4e27de8ddc416
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-25 20:35:41 +0600

    1001

--- beginner/1001/Main.java
+++ beginner/1001/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        try (Scanner myObj = new Scanner(System.in)) {
+            int A = myObj.nextInt();
+            int B = myObj.nextInt();
+            int X = A + B;
 
-        int A = myObj.nextInt();
-        int B = myObj.nextInt();
-        int X = A + B;
-
-        System.out.println("X = " + X);
+            System.out.println("X = " + X);
+        }
     }
 }
