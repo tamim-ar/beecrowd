9ee17e0a1e9bf815f42d3d59860dde8d5b1fff24
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-02-25 00:03:13 +0600

    2006

--- beginner/python/2006.py
+++ beginner/python/2006.py
@@ -1,11 +1,7 @@
-# Read the actual tea type
 T = int(input())
 
-# Read the contestants' answers
 answers = list(map(int, input().split()))
 
-# Count the number of correct answers
 correct_count = answers.count(T)
 
-# Print the result
 print(correct_count)
