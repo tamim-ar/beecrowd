b5b6e47257f3d0719925c227f628aec8f5ef69f4
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-04-20 23:44:42 +0600

    Remove unused input handling and comments from 1000.js

--- javascript/1000.js
+++ javascript/1000.js
@@ -1,9 +1,1 @@
-// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-// var lines = input.split('\n');
-
-/**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
- */
 console.log("Hello World!");
