427706ecfd8a36c806b27e6df0adaba6bb3b09cb
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-04-20 23:46:25 +0600

    Remove unnecessary comments from multiple Java files

--- java/1013.java
+++ java/1013.java
@@ -2,22 +2,10 @@ import java.util.*;
 
 import java.io.IOException;
  
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
  
     public static void main(String[] args) throws IOException {
  
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
-         
          Scanner sc = new Scanner(System.in);
          
          int a = sc.nextInt();
