d1df4798fceb9c24f8e3116be638e3803a3b7ac4
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-09-03 19:00:22 +0600

    1031. Power Crisis

--- solutions/1031. Power Crisis/1031.py
+++ solutions/1031. Power Crisis/1031.py
@@ -1,18 +1,15 @@
-def solve(N):
-    def josephus(n, k):
-        res = 0
-        for i in range(1, n+1):
-            res = (res + k) % i
-        return res
-
+def solve(n):
     m = 1
     while True:
-        if josephus(N-1, m) == 12:  
+        pos = 0
+        for i in range(2, n):   # simulate elimination of (n-1) regions after removing region 1 first
+            pos = (pos + m) % i
+        if pos == 11:  # Wellington (13) should be last → index 12 overall, but after removing region 1 it's 11
             return m
         m += 1
 
 while True:
-    N = int(input())
-    if N == 0:
+    n = int(input())
+    if n == 0:
         break
-    print(solve(N))
+    print(solve(n))
